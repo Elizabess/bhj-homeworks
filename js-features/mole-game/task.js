917af5b518783a9707401098aa45817f232e0612
Elizaveta <elizabess27@mail.ru>
2024-07-01 14:15:04 +0300

    Третье задание готово

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,32 @@
+
+ let counterDead = 0;
+ let counterLost = 0;
+getHole = index => document.getElementById(`hole${index}`);
+for ( holeIndex = 1; holeIndex < 10; holeIndex++ ) {
+    let hole = getHole(holeIndex);
+    hole.addEventListener( 'click', function() {
+        if ( hole.classList.contains( 'hole_has-mole')){
+            counterDead++;
+            dead.textContent = counterDead;
+        } else {
+            counterLost++;
+            lost.textContent = counterLost;
+        }
+ 
+        if (counterDead == 10) {
+            alert ("Победа!");
+            counterDead = 0;
+            counterLost = 0;
+            dead.textContent = counterDead;
+        }
+
+        if (counterLost == 5) {
+            alert("Вы проиграли!")
+            counterDead = 0;
+            counterLost = 0;
+            dead.textContent = counterLost;
+        }
+    });
+ 
+}
+replaceMole();
